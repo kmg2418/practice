240fcb7765bf85570b2da6850bd443cbb0e80958
lip <46185775+kmg2418@users.noreply.github.com>
2019-01-28 15:03:12 +0900

    04.01901281502

--- practice.js
+++ practice.js
@@ -1,31 +1,26 @@
 var links = {
   setColor:function(color){
-    var alist = document.querySelectorAll('a');
-    for(var i = 0; i < alist.length; i++){
-      alist[i].style.color = color;
-    }
-  }
+    $('a').css('color', color);
+   }
 }
 var body = {
   setBackgroundColor:function(color){
-    document.querySelector('body').style.backgroundColor = color;
+    $('body').css('backgroundColor', color);
   }
 }
 var grid = {
   setColor:function(color){
-    document.querySelector('#grid').style.color=color;
+    $('#grid').css('color', color);
   }
 }
 var ol = {
   setBorderColor:function(color){
-    document.querySelector('ol').style.borderColor=color;
+    $('ol').css('borderColor', color);
   }
 }
 var span = {
   setBorderColor:function(color){
-    for(var i = 0; i < document.querySelectorAll('span').length; i++){
-      document.querySelectorAll('span')[i].style.borderColor=color;
-    }
+    $('span').css('borderColor', color);
   }
 }
   function nightAndDay(obj) {
@@ -35,17 +30,13 @@ var span = {
       ol.setBorderColor('white');
       span.setBorderColor('white');
       links.setColor('white');
-      for(var i = 0; i < document.querySelectorAll('#NAD').length; i++){
-        document.querySelectorAll('#NAD')[i].value='day'
-      }
+      $("[id='NAD']").val('day')
     }else if(obj.value=='day'){
       body.setBackgroundColor('white');
       grid.setColor('black');
       ol.setBorderColor('black');
       span.setBorderColor('black');
       links.setColor('black');
-      for(var i = 0; i < document.querySelectorAll('#NAD').length; i++){
-        document.querySelectorAll('#NAD')[i].value='night'
-      }
+      $("[id='NAD']").val('night')
     }
   }
