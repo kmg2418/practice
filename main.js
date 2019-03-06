eb6f2002484fb747b17dbb24e39e2edec3afed17
lip <46185775+kmg2418@users.noreply.github.com>
2019-03-06 11:40:25 +0900

    홈페이지 구현

--- main.js
+++ main.js
@@ -8,54 +8,99 @@ var app = http.createServer(function(request,response){
     var pathname = url.parse(_url,true).pathname;
     var title = 'WEB 2 - NODE.JS';
     var head = queryData.id;
-    var txtname = queryData.id;
+    var textname = queryData.id;
 
     if (pathname === '/') {
-        fs.readFile(`Data/${txtname}`, 'utf8', function(err, description){
-            var template = `
-            <!DOCTYPE html>
-            <html>
-              <head>
-                <meta charset="utf-8">
-                <title>${title}</title>
-                <link rel="stylesheet" href="css/practice.css">
-                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-                <script src="js/practice.js"></script>
-              </head>
-              <body>
-                <header>
-                  <h1><a href="/">WEB</a></h1>
-                </header>
-                <span class="top">
-                  <input type="button" id="NAD" value="night" onclick="nightAndDay(this)">
-                </span>
-                <div id="grid">
-                  <ol>
-                    <li><a href="/?id=HTML">HTML</a></li>
-                    <li><a href="/?id=CSS">CSS</a></li>
-                    <li><a href="/?id=JavaScript">JavaScript</a></li>
-                  </ol>
-                  <div id="article">
-                    <h2>${head}</h2>
-                    <p>${description}</p>
-                  </div>
-                </div>
-                <span class="bottom">
-                  <input type="button" id="NAD" value="night" onclick="nightAndDay(this)">
-                </span>
-                <footer>
-                  <h3><a href="index.html">WEB</a></h3>
-                </footer>
-              </body>
-            </html>
-            `;
-            response.writeHead(200);
-            response.end(template);
-        });
+        if (queryData.id === undefined) {
+            head = 'Welcome';
+            var description = 'Hello, Node.js';
+            fs.readFile(`Data/${textname}`, 'utf8', function(err, description){
+                var template = `
+                <!DOCTYPE html>
+                <html>
+                  <head>
+                    <meta charset="utf-8">
+                    <title>${title}</title>
+                    <link rel="stylesheet" href="css/practice.css">
+                    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+                    <script src="js/practice.js"></script>
+                  </head>
+                  <body>
+                    <header>
+                      <h1><a href="/">WEB</a></h1>
+                    </header>
+                    <span class="top">
+                      <input type="button" id="NAD" value="night" onclick="nightAndDay(this)">
+                    </span>
+                    <div id="grid">
+                      <ol>
+                        <li><a href="/?id=HTML">HTML</a></li>
+                        <li><a href="/?id=CSS">CSS</a></li>
+                        <li><a href="/?id=JavaScript">JavaScript</a></li>
+                      </ol>
+                      <div id="article">
+                        <h2>${head}</h2>
+                        <p>${description}</p>
+                      </div>
+                    </div>
+                    <span class="bottom">
+                      <input type="button" id="NAD" value="night" onclick="nightAndDay(this)">
+                    </span>
+                    <footer>
+                      <h3><a href="index.html">WEB</a></h3>
+                    </footer>
+                  </body>
+                </html>
+                `;
+                response.writeHead(200);
+                response.end(template);
+            });
+        } else {
+            fs.readFile(`Data/${textname}`, 'utf8', function(err, description){
+                var template = `
+                <!DOCTYPE html>
+                <html>
+                  <head>
+                    <meta charset="utf-8">
+                    <title>${title}</title>
+                    <link rel="stylesheet" href="css/practice.css">
+                    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+                    <script src="js/practice.js"></script>
+                  </head>
+                  <body>
+                    <header>
+                      <h1><a href="/">WEB</a></h1>
+                    </header>
+                    <span class="top">
+                      <input type="button" id="NAD" value="night" onclick="nightAndDay(this)">
+                    </span>
+                    <div id="grid">
+                      <ol>
+                        <li><a href="/?id=HTML">HTML</a></li>
+                        <li><a href="/?id=CSS">CSS</a></li>
+                        <li><a href="/?id=JavaScript">JavaScript</a></li>
+                      </ol>
+                      <div id="article">
+                        <h2>${head}</h2>
+                        <p>${description}</p>
+                      </div>
+                    </div>
+                    <span class="bottom">
+                      <input type="button" id="NAD" value="night" onclick="nightAndDay(this)">
+                    </span>
+                    <footer>
+                      <h3><a href="index.html">WEB</a></h3>
+                    </footer>
+                  </body>
+                </html>
+                `;
+                response.writeHead(200);
+                response.end(template);
+            });
+        }
     } else {
         response.writeHead(404);
-        response.end("Not Found")
+        response.end("Not Found");
     }
-
 });
 app.listen(80);
